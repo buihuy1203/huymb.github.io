63eab1629ae63b4ccd1566fe693932f8691e4b7d
buihuy1203 <buihuy1203@gmail.com>
2024-11-20 01:27:09 +0700

    Download Resume

--- scripts.js
+++ scripts.js
@@ -116,3 +116,25 @@ window.addEventListener('click', function(event) {
         }
     });
 });
+
+// Get the button:
+let scrollbutton = document.getElementById("scroll");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    scrollbutton.style.opacity = 1;
+  } else {
+    scrollbutton.style.opacity = 0;
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  scrollbutton.style.opacity = 0;
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+document.getElementById('resume').addEventListener('click', function(event) {
+    event.preventDefault();  // Ngừng chuyển hướng trang
+}
